81d8b67e7bd6e68495864e6c2bde40f474de43e5
Francisco Martinez <franmnfc@gmail.com>
2025-12-15 00:11:15 -0300

    Fix Astro GitHub Pages deployment

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,6 +3,6 @@ import { defineConfig } from "astro/config";
 export default defineConfig({
   site: "https://franciscomarttinnez.github.io",
   base: "/PortfolioFranciscoMartinez/",
-  outDir: "docs",
-  output: "static"
+  output: "static",
+  outDir: "docs"
 });
